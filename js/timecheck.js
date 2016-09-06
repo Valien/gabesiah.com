f13c99ffc7a48459921116e539151eca9c623819
Allen Vailliencourt <chumpmonkey@gmail.com>
2016-09-06 16:40:55 -0400

    Greeting translation update.

--- js/timecheck.js
+++ js/timecheck.js
@@ -1,12 +1,17 @@
 function timeCheckGreeting() {
     var greeting;
+    var translation;
     var time = new Date().getHours();
     if (time < 12) {
         greeting = "Magandang Umaga!";
+        translation = "(good morning!)";
     } else if (time < 17) {
         greeting = "Magandang Hapon!";
+        translation = "(good afternoon)";
     } else {
         greeting = "Magandang Gabi!";
+        translation = "(good evening)";
     }
 document.getElementById("greeting").innerHTML = greeting;
+document.getElementById("translation").innerHTML = translation;
 }
